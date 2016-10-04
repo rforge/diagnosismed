14a56ba107254dec74d8019210aeb5dadacabbdc
tak101 <tak101@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2016-10-04 11:41:16 +0000

    Help files extended with a page for name UncertainInterval
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/diagnosismed@43 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/Branches/UA1 sep 2016/R/UncertainInterval.R
@@ -0,0 +1,10 @@
+#' @title Set of functions for the determination of an Uncertain Interval of test scores
+#'
+#' @description A collection of functions to determine a range of test scores that are inconclusive and do not allow a diagnosis (other than Uncertain) and to access its qualities.
+#'
+#' @seealso \code{\link{uncertain.interval}},  \code{\link{plotMD}},  \code{\link{get.intersection}},  \code{\link{quality.threshold}},  \code{\link{quality.threshold.uncertain}}
+#' @name UncertainInterval
+NULL
+#> NULL
+## "_PACKAGE"
+##> [1] "_PACKAGE"
